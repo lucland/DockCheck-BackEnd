f73729bd87fc2f2388facb61e91e8fc36916abb9
lucland <lucas.vsc@gmail.com>
2023-12-06 15:11:45 -0300

    Add console logs for supervisor controller functions

--- src/controllers/supervisorController.js
+++ src/controllers/supervisorController.js
@@ -23,11 +23,13 @@ exports.createSupervisor = async (req, res) => {
     const supervisorRef = db.collection('supervisors').doc(newSupervisor.id);
     await supervisorRef.set(supervisorData);
 
+    console.log("201 - supervisor created successfully");
     res.status(201).json({
       message: 'Supervisor created successfully',
       supervisor: newSupervisor,
     });
   } catch (error) {
+    console.log("400 - error creating supervisor");
     res.status(400).json({ message: 'Error creating supervisor', error });
   }
 };
@@ -37,10 +39,13 @@ exports.getSupervisor = async (req, res) => {
   try {
     const supervisor = await Supervisor.findByPk(req.params.id);
     if (!supervisor) {
+      console.log("404 - supervisor not found");
       return res.status(404).json({ message: 'Supervisor not found' });
     }
+    console.log("200 - supervisor fetched successfully");
     res.status(200).json(supervisor);
   } catch (error) {
+    console.log("400 - error fetching supervisor");
     res.status(400).json({ message: 'Error fetching supervisor', error });
   }
 };
@@ -51,6 +56,7 @@ exports.updateSupervisor = async (req, res) => {
     const { password, ...updateData } = req.body;
     const supervisor = await Supervisor.findByPk(req.params.id);
     if (!supervisor) {
+      console.log("404 - supervisor not found");
       return res.status(404).json({ message: 'Supervisor not found' });
     }
 
@@ -70,8 +76,10 @@ exports.updateSupervisor = async (req, res) => {
     const supervisorRef = db.collection('supervisors').doc(req.params.id);
     await supervisorRef.update(updateData);
 
+    console.log("200 - supervisor updated successfully");
     res.status(200).json(updatedSupervisor);
   } catch (error) {
+    console.log("400 - error updating supervisor");
     res.status(400).json({ message: 'Error updating supervisor', error });
   }
 };
@@ -81,6 +89,7 @@ exports.updateSupervisor = async (req, res) => {
     try {
       const supervisor = await Supervisor.findByPk(req.params.id);
       if (!supervisor) {
+        console.log("404 - supervisor not found");
         return res.status(404).json({ message: 'Supervisor not found' });
       }
   
@@ -91,8 +100,10 @@ exports.updateSupervisor = async (req, res) => {
       const supervisorRef = db.collection('supervisors').doc(req.params.id);
       await supervisorRef.delete();
   
+      console.log("204 - supervisor deleted successfully");
       res.status(204).json({ message: 'Supervisor deleted successfully' });
     } catch (error) {
+      console.log("400 - error deleting supervisor");
       res.status(400).json({ message: 'Error deleting supervisor', error });
     }
   };
@@ -101,8 +112,10 @@ exports.updateSupervisor = async (req, res) => {
 exports.getAllSupervisors = async (req, res) => {
   try {
     const supervisors = await Supervisor.findAll();
+    console.log("200 - supervisors fetched successfully");
     res.status(200).json(supervisors);
   } catch (error) {
+    console.log("400 - error fetching supervisors");
     res.status(400).json({ message: 'Error fetching supervisors', error });
   }
 };
