f5a3595d656d04a0a031c1150d568727aa3d6c5f
lucland <lucas.vsc@gmail.com>
2023-12-06 15:11:17 -0300

    Add console.log statements for error handling in authorizationController

--- src/controllers/authorizationController.js
+++ src/controllers/authorizationController.js
@@ -3,8 +3,10 @@ const Authorization = require('../models/Authorization');
 exports.createAuthorization = async (req, res) => {
   try {
     const authorization = await Authorization.create(req.body);
+    console.log("201 - authorization created successfully");
     res.status(201).json(authorization);
   } catch (error) {
+    console.log("400 - error creating authorization");
     res.status(400).json({ message: 'Error creating authorization', error });
   }
 };
@@ -15,8 +17,10 @@ exports.getAuthorizations = async (req, res) => {
       const authorizations = await Authorization.findAll({
         where: { user_id: userId }
       });
+      console.log("200 - authorizations fetched successfully");
       res.status(200).json(authorizations);
     } catch (error) {
+      console.log("400 - error fetching authorizations");
       res.status(400).json({ message: 'Error fetching authorizations', error });
     }
   };
@@ -25,10 +29,13 @@ exports.getAuthorizationById = async (req, res) => {
   try {
     const authorization = await Authorization.findByPk(req.params.id);
     if (!authorization) {
+      console.log("404 - authorization not found");
       return res.status(404).json({ message: 'Authorization not found' });
     }
+    console.log("200 - authorization fetched successfully");
     res.status(200).json(authorization);
   } catch (error) {
+    console.log("400 - error fetching authorization");
     res.status(400).json({ message: 'Error fetching authorization', error });
   }
 };
@@ -37,11 +44,14 @@ exports.updateAuthorization = async (req, res) => {
   try {
     const authorization = await Authorization.findByPk(req.params.id);
     if (!authorization) {
+      console.log("404 - authorization not found");
       return res.status(404).json({ message: 'Authorization not found' });
     }
     const updatedAuthorization = await authorization.update(req.body);
+    console.log("200 - authorization updated successfully");
     res.status(200).json(updatedAuthorization);
   } catch (error) {
+    console.log("400 - error updating authorization");
     res.status(400).json({ message: 'Error updating authorization', error });
   }
 };
@@ -53,8 +63,10 @@ exports.deleteAuthorization = async (req, res) => {
       return res.status(404).json({ message: 'Authorization not found' });
     }
     await authorization.destroy();
+    console.log("204 - authorization deleted successfully");
     res.status(204).json({});
   } catch (error) {
+    console.log("400 - error deleting authorization");
     res.status(400).json({ message: 'Error deleting authorization', error });
   }
 };
@@ -62,8 +74,10 @@ exports.deleteAuthorization = async (req, res) => {
 exports.getAllAuthorizations = async (req, res) => {
   try {
     const authorizations = await Authorization.findAll();
+    console.log("200 - authorizations fetched successfully");
     res.status(200).json(authorizations);
   } catch (error) {
+    console.log("400 - error fetching authorizations");
     res.status(400).json({ message: 'Error fetching authorizations', error });
   }
 };
@@ -74,8 +88,10 @@ exports.getAllAuthorizationIds = async (req, res) => {
     const authorizations = await Authorization.findAll({
       attributes: ['id']
     });
+    console.log("200 - authorizations fetched successfully");
     res.status(200).json(authorizations);
   } catch (error) {
+    console.log("400 - error fetching authorizations");
     res.status(400).json({ message: 'Error fetching authorizations', error });
   }
 };
