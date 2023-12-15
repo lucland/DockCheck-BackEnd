eaadb029c945a4d07b6d7dc7e3b19fa3aa8eed62
lucland <lucas.vsc@gmail.com>
2023-12-15 17:09:32 -0300

    Add status field to Login model

--- src/models/Login.js
+++ src/models/Login.js
@@ -14,6 +14,7 @@ class Login extends Model {
       system: DataTypes.STRING,
       created_at: DataTypes.DATE,
       updated_at: DataTypes.DATE,
+      status: DataTypes.STRING,
     }, {
       sequelize,
       modelName: 'Login',
