00e25560cfb531580666f987f996d0045dd62cdf
lucland <lucas.vsc@gmail.com>
2023-12-19 18:07:29 -0300

    Refactor createEvent function and add error handling

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,44 +1,46 @@
 const Event = require('../models/Event');
+const Vessel = require('../models/Vessel');
+const User = require('../models/User');
 const admin = require('../firebase'); // Importing from src/firebase.js
 const db = admin.firestore();
 
-// Create a new event
-exports.createEvent = async (req, res) => {
-  try {
-    // Save to PostgreSQL
-    const newEvent = await Event.create(req.body);
-
-    // Save to Firebase
-    const eventRef = db.collection('events').doc(); // Create a new doc with auto-generated ID
-    await eventRef.set({
-      id: eventRef.id, // Use auto-generated ID from Firebase
-      ...req.body
-    });
-
-    //update vessel.onboarded_count in PostgreSQL and Firebase incrementally if direction of event is 1 or decrementally if direction of event is 0
-    const vessel = await Vessel.findByPk(req.body.vessel_id);
-    if (req.body.direction == 0) {
-      vessel.onboarded_count = vessel.onboarded_count + 1;
-    } else if (req.body.direction == 1) {
-      vessel.onboarded_count = vessel.onboarded_count - 1;
-    } else {
-      console.log("400 - error updating vessel onboarded_count");
-      return res.status(400).json({ message: 'Error updating vessel onboarded_count' });
-    }
-
-    const user = await User.findByPk(req.body.user_id);
-    user.events.push(req.body.id);
-
-    console.log("201 - event created successfully");
-    res.status(201).json({
-      message: 'Event created successfully',
-      event: newEvent,
-    });
-  } catch (error) {
-    console.log("400 - error creating event");
-    res.status(400).json({ message: 'Error creating event', error });
-  }
-};
+    exports.createEvent = async (req, res) => {
+      try {
+        const newEvent = await Event.create(req.body);
+        console.log("Event created in PostgreSQL");
+    
+        const vessel = await Vessel.findByPk(req.body.vessel_id);
+        if (vessel) {
+          if (req.body.action == 1) {
+            vessel.onboarded_count += 1;
+          } else if (req.body.action == 2) {
+            vessel.onboarded_count -= 1;
+          }
+          await vessel.save();
+          console.log("Vessel onboarded_count updated");
+        } else {
+          console.log("Vessel not found");
+        }
+    
+        const user = await User.findByPk(req.body.user_id);
+        if (user) {
+          const currentEvents = user.events || [];
+          const updatedEvents = [...currentEvents, newEvent.id]; // Create a new array with the new event ID
+    
+          user.set('events', updatedEvents); // Explicitly set the updated events array
+          await user.save();
+    
+          console.log("User events updated with new event ID:", updatedEvents);
+    
+          res.status(201).json({ message: 'Event created successfully', event: newEvent });
+        } else {
+          console.log("User not found");
+        }
+      } catch (error) {
+        console.error("400 - error creating event", error);
+        res.status(400).json({ message: 'Error creating event', error: error.message });
+      }
+    };
 
 // Get an event by ID
 exports.getEvent = async (req, res) => {
@@ -143,7 +145,6 @@ exports.getEventsByUser = async (req, res) => {
     res.status(400).json({ message: 'Error fetching events', error });
   }
 };
-
 
 exports.syncEvents = async (req, res) => {
   try {
