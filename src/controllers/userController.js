1085c3b43e572c4f99aec2bb034ef597de441b6c
lucland <lucas.vsc@gmail.com>
2024-01-02 14:53:15 -0300

    new get by rfid method added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -422,3 +422,21 @@ exports.getApprovedUserIds = async (req, res) => {
   }
 };
 
+exports.getUserByRfid = async (req, res) => {
+  try {
+      const rfid = req.params.rfid;
+      const user = await User.findOne({ where: { rfid: rfid } });
+
+      if (!user) {
+          console.log("404 - User not found");
+          return res.status(404).json({ message: 'User not found' });
+      }
+
+      console.log("200 - User found");
+      res.status(200).json(user);
+  } catch (error) {
+      console.error("400 - Error fetching user", error);
+      res.status(400).json({ message: 'Error fetching user', error });
+  }
+};
+
