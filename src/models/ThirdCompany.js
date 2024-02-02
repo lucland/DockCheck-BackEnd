6d1c94379edf95d6c62f3b5026bef890fddcd7d3
lucland <lucas.vsc@gmail.com>
2024-02-02 17:09:02 -0300

    Add ThirdCompany model class

---
+++ src/models/ThirdCompany.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+
+class ThirdCompany extends Model {
+  static init(sequelize) {
+    super.init({
+      name: DataTypes.STRING,
+      logo: DataTypes.STRING,
+      supervisors: DataTypes.ARRAY(DataTypes.STRING),
+      razao_social: DataTypes.STRING,
+      cnpj: DataTypes.STRING,
+      address: DataTypes.STRING,
+      updated_at: DataTypes.DATE,
+      is_vessel_company: DataTypes.BOOLEAN,
+      status: DataTypes.STRING,
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+    }, {
+      sequelize,
+      modelName: 'ThirdCompany',
+    });
+  }
+}
+
+module.exports = ThirdCompany;
