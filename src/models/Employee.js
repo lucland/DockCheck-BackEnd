6d84c89f7516d51e9efb4750f5a983a49a2c3873
lucland <lucas.vsc@gmail.com>
2024-02-02 17:08:30 -0300

    Add Employee model class

---
+++ src/models/Employee.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Employee extends Model {
+  static init(sequelize) {
+    super.init({
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING,
+      third_company: DataTypes.STRING,
+      visitor_company: DataTypes.STRING,
+      role: DataTypes.STRING,
+      project: DataTypes.STRING,
+      number: DataTypes.INTEGER,
+      blood_type: DataTypes.STRING,
+      cpf: DataTypes.STRING,
+      email: DataTypes.STRING,
+      area: DataTypes.STRING,
+      last_area_found: DataTypes.STRING,
+      last_time_found: DataTypes.DATE,
+      is_blocked: DataTypes.BOOLEAN,
+      block_reason: DataTypes.STRING,
+      status: DataTypes.STRING,
+    }, {
+      sequelize,
+      modelName: 'Employee',
+    });
+  }
+}
+
+module.exports = Employee;
