0c34b45d63593a93b997aabea7966cec80b7971c
lucland <lucas.vsc@gmail.com>
2024-02-02 17:12:17 -0300

    Refactor Vessel model to update field names and remove unused fields

--- src/models/Vessel.js
+++ src/models/Vessel.js
@@ -3,17 +3,14 @@ const { Model, DataTypes } = require('sequelize');
 class Vessel extends Model {
   static init(sequelize) {
     super.init({
-      name: DataTypes.STRING,
-      company_id: DataTypes.STRING,
-      updated_at: DataTypes.DATE,
       id: {
         type: DataTypes.STRING,
         primaryKey: true,
       },
-      admins: DataTypes.ARRAY(DataTypes.STRING),
+      name: DataTypes.STRING,
+      company_id: DataTypes.STRING,
       onboarded_count: DataTypes.INTEGER,
-      portals: DataTypes.ARRAY(DataTypes.STRING),
-      onboarded_users: DataTypes.ARRAY(DataTypes.STRING),
+      areas: DataTypes.ARRAY(DataTypes.STRING),
       status: DataTypes.STRING,
     }, {
       sequelize,
