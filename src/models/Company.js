60ad9fd16531f3974fb46fc439c2a3551cc0a75b
lucland <lucas.vsc@gmail.com>
2024-02-06 16:18:20 -0300

    Update Company model fields

--- src/models/Company.js
+++ src/models/Company.js
@@ -5,12 +5,14 @@ class Company extends Model {
     super.init({
       name: DataTypes.STRING,
       logo: DataTypes.STRING,
-      supervisors: DataTypes.ARRAY(DataTypes.STRING),
-      crew: DataTypes.ARRAY(DataTypes.STRING),
-      projects: DataTypes.ARRAY(DataTypes.STRING),
-      admins: DataTypes.ARRAY(DataTypes.STRING),
+      crew_id: DataTypes.ARRAY(DataTypes.STRING),
+      projects_id: DataTypes.ARRAY(DataTypes.STRING),
+      admins_id: DataTypes.ARRAY(DataTypes.STRING),
       razao_social: DataTypes.STRING,
       cnpj: DataTypes.STRING,
+      address: DataTypes.STRING,
+      telephone: DataTypes.STRING,
+      email: DataTypes.STRING,
       status: DataTypes.STRING,
       id: {
         type: DataTypes.STRING,
