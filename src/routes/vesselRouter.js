a80c1e9c82b2ca3f5dd7d525edd7a321c30ab248
lucland <lucas.vsc@gmail.com>
2024-02-07 16:39:10 -0300

    Refactor vesselRouter.js routes

--- src/routes/vesselRouter.js
+++ src/routes/vesselRouter.js
@@ -6,30 +6,21 @@ const authenticateJWT = require('../middleware/auth');
 
 router.post('/create', authenticateJWT, vesselController.createVessel);
 
-
 router.get('/:id', authenticateJWT, vesselController.getVessel);
-
 
 router.put('/:id', authenticateJWT, vesselController.updateVessel);
 
-
 router.delete('/:id', authenticateJWT, vesselController.deleteVessel);
-
 
 router.get('/company/:company_id', authenticateJWT, vesselController.getVesselsByCompany);
 
-
 router.get('/', authenticateJWT, vesselController.getAllVessels);
-
 
 router.get('/name/:vessel_name', authenticateJWT, vesselController.getVesselByName);
 
-
 router.get('/ids', authenticateJWT, vesselController.getAllVesselIds);
 
-
 router.get('/onboarded/:id', authenticateJWT, vesselController.getOnboardedUsers);
-
 
 router.get('/events/:vessel_id', authenticateJWT, vesselController.getEventsByVessel);
 
