7e7985b93cd10eebc3941225f8cf8845984d135a
lucland <lucas.vsc@gmail.com>
2024-02-07 13:52:11 -0300

    Add sequelize import and add indexes to Event model fields

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
 class Event extends Model {
   static init(sequelize) {
@@ -7,12 +8,23 @@ class Event extends Model {
         type: DataTypes.STRING,
         primaryKey: true,
       },
-      employee_id: DataTypes.STRING,
-      timestamp: DataTypes.DATE,
-      project_id: DataTypes.STRING,
-      area_id: DataTypes.STRING,
+      employee_id: {
+        type: DataTypes.STRING,
+        index: true,
+      },
+      timestamp:  {
+        type: DataTypes.DATE,
+        index: true,
+      },
+      project_id: {
+        type: DataTypes.STRING,
+        index: true,
+      },
       action: DataTypes.INTEGER,
-      sensor_id: DataTypes.STRING,
+      sensor_id:{
+        type: DataTypes.STRING,
+        index: true,
+      },
       status: DataTypes.STRING,
       beacon_id: DataTypes.STRING,
     }, {
