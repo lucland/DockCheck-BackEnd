4447a866a2bc62edd6626c8b93c9acf66b26f64b
lucland <lucas.vsc@gmail.com>
2024-02-07 13:53:34 -0300

    Add Swagger documentation and routes for third project CRUD operations

--- src/routes/thirdProjectRouter.js
+++ src/routes/thirdProjectRouter.js
@@ -1,1 +1,176 @@
-//create third project router with swagger documentation
+const express = require('express');
+const thirdProjectController = require('../controllers/thirdProjectController');
+const router = express.Router();
+const authenticateJWT = require('../middleware/auth');
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects:
+ *  post:
+ *    summary: Create a new third project
+ *    tags: [ThirdProjects]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/ThirdProject'
+ *    responses:
+ *      '201':
+ *        description: Third project created successfully
+ *      '400':
+ *        description: Bad request
+ */
+router.post('/', authenticateJWT, thirdProjectController.createThirdProject);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects/{id}:
+ *  get:
+ *    summary: Get a third project by ID
+ *    tags: [ThirdProjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: Successful operation
+ *      '404':
+ *        description: Third project not found
+ *      '400':
+ *        description: Bad request
+ */
+router.get('/:id', authenticateJWT, thirdProjectController.getThirdProject);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects:
+ *  get:
+ *    summary: Get all third projects
+ *    tags: [ThirdProjects]
+ *    responses:
+ *      '200':
+ *        description: Successful operation
+ *      '400':
+ *        description: Bad request
+ */
+router.get('/', authenticateJWT, thirdProjectController.getAllThirdProjects);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects/{id}:
+ *  put:
+ *    summary: Update a third project by ID
+ *    tags: [ThirdProjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/ThirdProject'
+ *    responses:
+ *      '200':
+ *        description: Third project updated successfully
+ *      '404':
+ *        description: Third project not found
+ *      '400':
+ *        description: Bad request
+ */
+router.put('/:id', authenticateJWT, thirdProjectController.updateThirdProject);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects/{id}:
+ *  delete:
+ *    summary: Delete a third project by ID
+ *    tags: [ThirdProjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: Third project deleted successfully
+ *      '404':
+ *        description: Third project not found
+ *      '400':
+ *        description: Bad request
+ */
+router.delete('/:id', authenticateJWT, thirdProjectController.deleteThirdProject);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects/{id}/addEmployee:
+ *  put:
+ *    summary: Add an employee to a third project
+ *    tags: [ThirdProjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              employee_id:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: Employee added to third project successfully
+ *      '400':
+ *        description: Bad request
+ *      '404':
+ *        description: Third project not found
+ */
+router.put('/:id/addEmployee', authenticateJWT, thirdProjectController.addEmployeeToThirdProject);
+
+/**
+ * @swagger
+ * /api/v1/thirdProjects/{id}/updateAllowedAreas:
+ *  put:
+ *    summary: Update allowed areas of a third project
+ *    tags: [ThirdProjects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              allowed_areas_id:
+ *                type: array
+ *                items:
+ *                  type: string
+ *    responses:
+ *      '200':
+ *        description: Allowed areas updated successfully
+ *      '400':
+ *        description: Bad request
+ *      '404':
+ *        description: Third project not found
+ */
+router.put('/:id/updateAllowedAreas', authenticateJWT, thirdProjectController.updateAllowedAreasOfThirdProject);
+
+module.exports = router;
