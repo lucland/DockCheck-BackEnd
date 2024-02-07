28d46d37609f4a876661e6e4f351a45087f39908
lucland <lucas.vsc@gmail.com>
2024-02-07 13:51:05 -0300

    Update employeeController.js with additional fields and add updateEmployeeArea function

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -46,12 +46,21 @@ exports.updateEmployee = async (req, res) => {
             console.log("404 - employee not found");
             return res.status(404).json({ message: 'Employee not found' });
         }
-        const { name, email, phone, updated_at, status } = req.body;
+        const { name, third_company_id, visitor_company, role, project_id, number, blood_type, cpf, area_id, last_area_found, last_time_found, is_blocked, block_reason, status } = req.body;
         await employee.update({
             name,
-            email,
-            phone,
-            updated_at,
+            third_company_id,
+            visitor_company,
+            role,
+            project_id,
+            number,
+            blood_type,
+            cpf,
+            area_id,
+            last_area_found,
+            last_time_found,
+            is_blocked,
+            block_reason,
             status,
         });
         console.log("200 - employee updated successfully");
@@ -75,5 +84,26 @@ exports.deleteEmployee = async (req, res) => {
     } catch (error) {
         console.log("400 - error deleting employee");
         res.status(400).json({ message: 'Error deleting employee', error });
+    }
+};
+
+//update last area found and last time found
+exports.updateEmployeeArea = async (req, res) => {
+    try {
+        const employee = await Employee.findByPk(req.params.id);
+        if (!employee) {
+            console.log("404 - employee not found");
+            return res.status(404).json({ message: 'Employee not found' });
+        }
+        const { last_area_found, last_time_found } = req.body;
+        await employee.update({
+            last_area_found,
+            last_time_found,
+        });
+        console.log("200 - employee updated successfully");
+        res.status(200).json(employee);
+    } catch (error) {
+        console.log("400 - error updating employee");
+        res.status(400).json({ message: 'Error updating employee', error });
     }
 };
