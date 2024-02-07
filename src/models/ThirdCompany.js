7277d55cd928c5f63250dead1ad1d6acf5e102fe
lucland <lucas.vsc@gmail.com>
2024-02-07 13:52:25 -0300

    Refactor ThirdCompany model in src/models/ThirdCompany.js

--- src/models/ThirdCompany.js
+++ src/models/ThirdCompany.js
@@ -1,23 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
 class ThirdCompany extends Model {
   static init(sequelize) {
     super.init({
-      name: DataTypes.STRING,
-      logo: DataTypes.STRING,
-      supervisors: DataTypes.ARRAY(DataTypes.STRING),
-      razao_social: DataTypes.STRING,
-      cnpj: DataTypes.STRING,
-      address: DataTypes.STRING,
-      updated_at: DataTypes.DATE,
-      is_vessel_company: DataTypes.BOOLEAN,
-      telephone: DataTypes.STRING,
-      email: DataTypes.STRING,
-      status: DataTypes.STRING,
       id: {
         type: DataTypes.STRING,
         primaryKey: true,
       },
+      name: DataTypes.STRING,
+      logo: DataTypes.STRING,
+      razao_social: DataTypes.STRING,
+      cnpj: DataTypes.STRING,
+      address: DataTypes.STRING,
+      is_vessel_company: DataTypes.BOOLEAN,
+      admins_id: DataTypes.ARRAY(DataTypes.STRING),
+      telephone: DataTypes.STRING,
+      email: DataTypes.STRING,
+      status: DataTypes.STRING,
     }, {
       sequelize,
       modelName: 'ThirdCompany',
