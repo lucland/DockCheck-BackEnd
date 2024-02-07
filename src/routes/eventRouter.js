6e20b0c0e055c8071ca68687663f3bd6bc148174
lucland <lucas.vsc@gmail.com>
2024-02-07 16:34:58 -0300

    Update user authentication logic

--- src/routes/eventRouter.js
+++ src/routes/eventRouter.js
@@ -1,298 +1,200 @@
 const express = require('express');
 const eventController = require('../controllers/eventController');
+
 const router = express.Router();
 
 /**
  * @swagger
- * /api/v1/events/create:
- *  post:
- *    summary: Create a new event
- *    tags: [Events]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Event'
- *    responses:
- *      '201':
- *        description: Event created successfully
- *        content:
- *          application/json:
- *            example:
- *              id: "event123"
- *              portal_id: "portal1"
- *              user_id: "user1"
- *              timestamp: "2023-10-25T00:00:00.000Z"
- *              direction: 1
- *              picture: "picture_url"
- *              vessel_id: "vessel123"
- *              action: 1
- *      '400':
- *        description: Bad request
- *        content:
- *          application/json:
- *            example:
- *              message: "Error creating event"
- *              error: "Details about the error"
- */
-router.post('/create', eventController.createEvent);
-
-/**
- * @swagger
- * /api/v1/events/{id}:
- *  get:
- *    summary: Get an event by ID
- *    tags: [Events]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: Event ID
- *    responses:
- *      '200':
- *        description: Successful operation
- *        content:
- *          application/json:
- *            example:
- *              id: "event123"
- *              portal_id: "portal1"
- *              user_id: "user1"
- *              timestamp: "2023-10-25T00:00:00.000Z"
- *              direction: 1
- *              picture: "picture_url"
- *              vessel_id: "vessel123"
- *              action: 1
- *      '404':
- *        description: Event not found
- *        content:
- *          application/json:
- *            example:
- *              message: "Event not found"
- */
-router.get('/:id', eventController.getEvent);
-
-/**
- * @swagger
- * /api/v1/events/{id}:
- *  put:
- *    summary: Update an event by ID
- *    tags: [Events]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: Event ID
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Event'
- *    responses:
- *      '200':
- *        description: Event updated successfully
- *        content:
- *          application/json:
- *            example:
- *              id: "event123"
- *              portal_id: "portal1"
- *              user_id: "user2"
- *              timestamp: "2023-10-25T00:00:00.000Z"
- *              direction: 2
- *              picture: "new_picture_url"
- *              vessel_id: "vessel123"
- *              action: 2
- *      '400':
- *        description: Bad request
- *        content:
- *          application/json:
- *            example:
- *              message: "Error updating event"
- *              error: "Details about the error"
- *      '404':
- *        description: Event not found
- *        content:
- *          application/json:
- *            example:
- *              message: "Event not found"
- */
-router.put('/:id', eventController.updateEvent);
-
-/**
- * @swagger
- * /api/v1/events/{id}:
- *  delete:
- *    summary: Delete an event by ID
- *    tags: [Events]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: Event ID
- *    responses:
- *      '204':
- *        description: Event deleted successfully
- *      '404':
- *        description: Event not found
- */
-router.delete('/:id', eventController.deleteEvent);
-
-/**
- * @swagger
  * /api/v1/events:
- *  get:
- *    summary: Get all events with pagination
- *    tags: [Events]
- *    parameters:
- *      - in: query
- *        name: limit
- *        schema:
- *          type: integer
- *        required: false
- *        description: Limit number of events. Default is 10.
- *      - in: query
- *        name: offset
- *        schema:
- *          type: integer
- *        required: false
- *        description: Starting index for events. Default is 0.
- *    responses:
- *      '200':
- *        description: Successful operation
- *        content:
- *          application/json:
- *            example:
- *              - id: "event123"
- *                portal_id: "portal1"
- *                user_id: "user1"
- *                timestamp: "2023-10-25T00:00:00.000Z"
- *                direction: 1
- *                picture: "picture_url"
- *                vessel_id: "vessel123"
- *                action: 1
- *              - id: "event124"
- *                portal_id: "portal2"
- *                user_id: "user2"
- *                timestamp: "2023-10-26T00:00:00.000Z"
- *                direction: 2
- *                picture: "another_picture_url"
- *                vessel_id: "vessel124"
- *                action: 2
- *      '400':
- *        description: Bad request
- *        content:
- *          application/json:
- *            example:
- *              message: "Error fetching events"
- *              error: "Details about the error"
+ *   post:
+ *     summary: Create a new event
+ *     tags: [Events]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - itag
+ *               - code
+ *               - action
+ *               - project_id
+ *               - timestamp
+ *             properties:
+ *               itag:
+ *                 type: string
+ *                 description: The ITAG string
+ *               code:
+ *                 type: integer
+ *                 description: The code of the sensor
+ *               action:
+ *                 type: integer
+ *                 description: The action of the event
+ *               project_id:
+ *                 type: string
+ *                 description: The project ID
+ *               timestamp:
+ *                 type: string
+ *                 format: date-time
+ *                 description: The timestamp of the event
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The ID of the created event
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Server error
  */
-router.get('/', eventController.getAllEvents);
+router.post('/', eventController.createEvent);
 
 /**
  * @swagger
- * /api/v1/events/sync:
- *  post:
- *    summary: Sync events
- *    tags: [Events]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            type: 'object'
- *            properties:
- *              events:
- *                type: 'array'
- *                items:
- *                  $ref: '#/components/schemas/Event'
- *    responses:
- *      '200':
- *        description: Events synced successfully
- *      '400':
- *        description: Bad request
- *      '500':
- *        description: Internal server error
+ * /api/v1/events/{employee_id}:
+ *   get:
+ *     summary: Get all events of a specific employee
+ *     tags: [Events]
+ *     parameters:
+ *       - in: path
+ *         name: employee_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           description: The ID of the employee
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: The ID of the event
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Server error
  */
-router.post('/sync', eventController.syncEvents);
+router.get('/:employee_id', eventController.getEventsByEmployeeId);
 
-//swagger documentation for get events by user
 /**
  * @swagger
- * /api/v1/events/user/{id}:
- *  get:
- *    summary: Get all events by user
- *    tags: [Events]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: User ID
- *    responses:
- *      '200':
- *        description: Successful operation
- *        content:
- *          application/json:
- *            example:
- *              - id: "event123"
- *                portal_id: "portal1"
- *                user_id: "user1"
- *                timestamp: "2023-10-25T00:00:00.000Z"
- *                direction: 1
- *                picture: "picture_url"
- *                vessel_id: "vessel123"
- *                action: 1
- *              - id: "event124"
- *                portal_id: "portal2"
- *                user_id: "user1"
- *                timestamp: "2023-10-26T00:00:00.000Z"
- *                direction: 2
- *                picture: "another_picture_url"
- *                vessel_id: "vessel124"
- *                action: 2
- *      '400':
- *        description: Bad request
- *        content:
- *          application/json:
- *            example:
- *              message: "Error fetching events"
- *              error: "Details about the error"
+ * /api/v1/events/single/{id}:
+ *   get:
+ *     summary: Get a specific event by ID
+ *     tags: [Events]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           description: The ID of the event
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The ID of the event
+ *                 employee_id:
+ *                   type: string
+ *                   description: The ID of the employee associated with the event
+ *                 timestamp:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The timestamp of the event
+ *                 project_id:
+ *                   type: string
+ *                   description: The ID of the project associated with the event
+ *                 action:
+ *                   type: integer
+ *                   description: The action of the event
+ *                 sensor_id:
+ *                   type: string
+ *                   description: The ID of the sensor associated with the event
+ *                 status:
+ *                   type: string
+ *                   description: The status of the event
+ *                 beacon_id:
+ *                   type: string
+ *                   description: The ID of the beacon associated with the event
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Server error
  */
-router.get('/user/:user_id', eventController.getEventsByUser);
+router.get('/single/:id', eventController.getEventById);
 
-//swagger documentation for get all ids
 /**
  * @swagger
- * /api/v1/events/ids:
- *  get:
- *    summary: Get all events ids
- *    tags: [Events]
- *    responses:
- *      '200':
- *        description: Successful operation
- *        content:
- *          application/json:
- *            example:
- *              - id: "event123"
- *              - id: "event124"
- *      '400':
- *        description: Bad request
- *        content:
- *          application/json:
- *            example:
- *              message: "Error fetching events"
- *              error: "Details about the error"
+ * /api/v1/events/specific:
+ *   post:
+ *     summary: Create a specific event
+ *     tags: [Events]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - itag
+ *               - code
+ *               - action
+ *               - project_id
+ *               - timestamp
+ *             properties:
+ *               itag:
+ *                 type: string
+ *                 description: The ITAG string
+ *               code:
+ *                 type: integer
+ *                 description: The code of the sensor
+ *               action:
+ *                 type: integer
+ *                 description: The action of the event
+ *               project_id:
+ *                 type: string
+ *                 description: The project ID
+ *               timestamp:
+ *                 type: string
+ *                 format: date-time
+ *                 description: The timestamp of the event
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The ID of the created event
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Server error
  */
-router.get('/ids', eventController.getEventsIds);
-
-router.post('/sync', eventController.syncEvents);
+router.post('/specific', eventController.createSpecificEvent);
 
 module.exports = router;
