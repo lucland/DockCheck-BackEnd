db1b90d716e158861c9faaef3c4828953a95ac65
lucland <lucas.vsc@gmail.com>
2024-02-07 13:52:14 -0300

    Add status field to Picture model

--- src/models/Picture.js
+++ src/models/Picture.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
 class Picture extends Model {
     static init(sequelize) {
@@ -10,6 +11,7 @@ class Picture extends Model {
         employee_id: DataTypes.STRING,
         base_64: DataTypes.TEXT,
         doc_path: DataTypes.STRING,
+        status: DataTypes.STRING,
         }, {
       sequelize,
       modelName: 'Picture',
