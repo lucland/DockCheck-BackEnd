8d4deef7c3d2de88eb95a35b333a68e8586c2cba
lucland <lucas.vsc@gmail.com>
2024-02-07 13:52:43 -0300

    Refactor authorizationRouter.js

--- src/routes/authorizationRouter.js
+++ src/routes/authorizationRouter.js
@@ -1,133 +1,203 @@
 const express = require('express');
-const {
-  createAuthorization,
-  getAuthorizations,
-  getAuthorizationById,
-  updateAuthorization,
-  deleteAuthorization,
-} = require('../controllers/authorizationController');
-
-const router = express.Router();
 const authorizationController = require('../controllers/authorizationController');
+const router = express.Router();
+const authenticateJWT = require('../middleware/auth');
 
 /**
  * @swagger
- * /api/v1/authorizations:
- *   post:
- *     summary: Create a new authorization
- *     tags: [Authorization]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Authorization'
- *     responses:
- *       201:
- *         description: Successfully created
+ * /api/v1/authorizations/employee:
+ *  post:
+ *    summary: Create a new employee authorization
+ *    tags: [Authorizations]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Authorization'
+ *    responses:
+ *      '201':
+ *        description: Authorization created successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Authorization'
+ *      '400':
+ *        description: Bad request
  */
-router.post('/', authorizationController.createAuthorization);
-
-/**
- * @swagger
- * /api/v1/authorizations/{user_id}:
- *   get:
- *     summary: Get all authorizations for a user
- *     tags: [Authorization]
- *     parameters:
- *       - in: path
- *         name: user_id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully fetched
- */
-router.get('/user/:user_id', authorizationController.getAuthorizations);
+router.post('/employee', authenticateJWT, authorizationController.createEmployeeAuthorization);
 
 /**
  * @swagger
  * /api/v1/authorizations/{id}:
- *   get:
- *     summary: Get an authorization by ID
- *     tags: [Authorization]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully fetched
+ *  get:
+ *    summary: Get an authorization by ID
+ *    tags: [Authorizations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: Successful operation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Authorization'
+ *      '404':
+ *        description: Authorization not found
+ *      '400':
+ *        description: Bad request
  */
-router.get('/:id', authorizationController.getAuthorizationById);
+router.get('/:id', authenticateJWT, authorizationController.getAuthorizationById);
 
 /**
  * @swagger
  * /api/v1/authorizations/{id}:
- *   put:
- *     summary: Update an authorization by ID
- *     tags: [Authorization]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Authorization'
- *     responses:
- *       200:
- *         description: Successfully updated
+ *  put:
+ *    summary: Update an authorization by ID
+ *    tags: [Authorizations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Authorization'
+ *    responses:
+ *      '200':
+ *        description: Authorization updated successfully
+ *      '404':
+ *        description: Authorization not found
+ *      '400':
+ *        description: Bad request
  */
-router.put('/:id', authorizationController.updateAuthorization);
+router.put('/:id', authenticateJWT, authorizationController.updateAuthorization);
 
 /**
  * @swagger
  * /api/v1/authorizations/{id}:
- *   delete:
- *     summary: Delete an authorization by ID
- *     tags: [Authorization]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully deleted
+ *  delete:
+ *    summary: Delete an authorization by ID
+ *    tags: [Authorizations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '204':
+ *        description: Authorization deleted successfully
+ *      '404':
+ *        description: Authorization not found
+ *      '400':
+ *        description: Bad request
  */
-router.delete('/:id', authorizationController.deleteAuthorization);
+router.delete('/:id', authenticateJWT, authorizationController.deleteAuthorization);
 
 /**
  * @swagger
  * /api/v1/authorizations:
  *  get:
- *   summary: Get all authorizations
- *  tags: [Authorization]
- * responses:
- *  200:
- *  description: Successfully fetched
+ *    summary: Get all authorizations
+ *    tags: [Authorizations]
+ *    responses:
+ *      '200':
+ *        description: Successful operation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Authorization'
+ *      '400':
+ *        description: Bad request
  */
-router.get('/', authorizationController.getAllAuthorizations);
+router.get('/', authenticateJWT, authorizationController.getAllAuthorizations);
 
 /**
  * @swagger
  * /api/v1/authorizations/ids:
  *  get:
- *   summary: Get all authorization ids
- *  tags: [Authorization]
- * responses:
- *  200:
- *  description: Successfully fetched
+ *    summary: Get all authorization IDs
+ *    tags: [Authorizations]
+ *    responses:
+ *      '200':
+ *        description: Successful operation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *      '400':
+ *        description: Bad request
  */
-router.get('/ids', authorizationController.getAllAuthorizationIds);
+router.get('/ids', authenticateJWT, authorizationController.getAllAuthorizationIds);
+
+/**
+ * @swagger
+ * /api/v1/authorizations/add-to-employee/{id}/{auth_id}:
+ *  put:
+ *    summary: Add an authorization to an employee
+ *    tags: [Authorizations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *      - in: path
+ *        name: auth_id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: Authorization added to employee successfully
+ *      '404':
+ *        description: Employee not found
+ *      '400':
+ *        description: Bad request
+ */
+router.put('/add-to-employee/:id/:auth_id', authenticateJWT, authorizationController.addAuthorizationToEmployee);
+
+/**
+ * @swagger
+ * /api/v1/authorizations/remove-from-employee/{id}/{auth_id}:
+ *  put:
+ *    summary: Remove an authorization from an employee
+ *    tags: [Authorizations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ *      - in: path
+ *        name: auth_id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: Authorization removed from employee successfully
+ *      '404':
+ *        description: Employee not found
+ *      '400':
+ *        description: Bad request
+ */
+router.put('/remove-from-employee/:id/:auth_id', authenticateJWT, authorizationController.removeAuthorizationFromEmployee);
 
 module.exports = router;
