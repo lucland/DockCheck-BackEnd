ce429bdc88e18ab0f6cc7299be01ec452a749a99
lucland <lucas.vsc@gmail.com>
2024-02-16 15:19:44 -0300

    Add CompanyAdmin model

---
+++ src/models/CompanyAdmin.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+class CompanyAdmin extends Model {
+    static init(sequelize) {
+      super.init({
+        company_id: {
+          type: DataTypes.STRING,
+          references: { model: 'companies', key: 'id' },
+        },
+        user_id: {
+          type: DataTypes.STRING,
+          references: { model: 'users', key: 'id' },
+        },
+      }, {
+        sequelize,
+        modelName: 'CompanyAdmin',
+        tableName: 'company_admins',
+        timestamps: false,
+      });
+    }
+    static associate(models) {
+        this.belongsTo(models.Company, { foreignKey: 'company_id' });
+        this.belongsTo(models.User, { foreignKey: 'user_id' });
+    }
+      
+}
+
+module.exports = CompanyAdmin;
