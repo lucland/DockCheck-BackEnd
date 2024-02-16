dc710fad91c0a5dc9b7e0fafc0d20bc84909d36d
lucland <lucas.vsc@gmail.com>
2024-02-16 15:21:08 -0300

    Add association between Vessel and Company models

--- src/models/Vessel.js
+++ src/models/Vessel.js
@@ -19,6 +19,10 @@ class Vessel extends Model {
       modelName: 'Vessel',
     });
   }
+  static associate(models) {
+    this.belongsTo(models.Company, { foreignKey: 'company_id' });
+  }
+  
 }
 
 module.exports = Vessel;
