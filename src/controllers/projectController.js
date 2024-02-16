67fc70b4f636ff8ace39d50e1abcff455ebc0096
lucland <lucas.vsc@gmail.com>
2024-02-16 15:19:25 -0300

    Add project, third company, admin, and area to project

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -6,10 +6,16 @@ const ThirdProject = require('../models/ThirdProject');
 const Employee = require('../models/Employee');
 const Document = require('../models/Document');
 const Beacon = require('../models/Beacon');
+const Company = require('../models/Company');
+const CompanyProject = require('../models/CompanyProject');
+const ProjectAdmin = require('../models/ProjectAdmin');
 
 exports.createProject = async (req, res) => {
     try {
         const project = await Project.create(req.body);
+        //add project to companyProject table
+         await CompanyProject.create({ company_id: req.body.company_id, project_id: project.id });
+
         console.log("201 - project created successfully");
         res.status(201).json(project);
     } catch (error) {
@@ -98,6 +104,8 @@ exports.addThirdCompany = async (req, res) => {
         const third_companies = project.third_companies;
         third_companies.push(req.body.third_company_id);
         await project.update({ third_companies });
+        //update thirdCompanyProject table
+        await CompanyProject.create({ company_id: req.body.third_company_id, project_id: project.id });
         console.log("200 - third company added successfully");
         res.status(200).json(project);
     } catch (error) {
@@ -144,5 +152,48 @@ exports.approvedEmployeesOfTheDay = async (req, res) => {
     } catch (error) {
         console.log("500 - error getting approved employees");
         res.status(500).json({ message: 'Error getting approved employees', error });
+    }
+};
+
+//add admin to project
+exports.addAdminToProject = async (req, res) => {
+    try {
+        const project = await Project.findByPk(req.params.id);
+        if (!project) {
+            console.log("404 - project not found");
+            return res.status(404).json({ message: 'Project not found' });
+        }
+        //add to ProjectAdmin table
+        await ProjectAdmin.create({ project_id: req.params.id, admin_id: req.body.admin_id });
+
+        const admins = project.admins;
+        admins.push(req.body.admin_id);
+        await project.update({ admins });
+        console.log("200 - admin added successfully");
+        res.status(200).json(project);
+    } catch (error) {
+        console.log("400 - error adding admin");
+        res.status(400).json({ message: 'Error adding admin', error });
+    }
+};
+
+//add area id to the project and update ProjectArea table
+exports.addAreaToProject = async (req, res) => {
+    try {
+        const project = await Project.findByPk(req.params.id);
+        if (!project) {
+            console.log("404 - project not found");
+            return res.status(404).json({ message: 'Project not found' });
+        }
+        const areas = project.areas;
+        areas.push(req.body.area_id);
+        await project.update({ areas });
+        //update ProjectArea table
+        await CompanyProject.create({ company_id: req.body.area_id, project_id: project.id });
+        console.log("200 - area added successfully");
+        res.status(200).json(project);
+    } catch (error) {
+        console.log("400 - error adding area");
+        res.status(400).json({ message: 'Error adding area', error });
     }
 };
