fb41651d6add6d52aaf1805d820fd3354500ba03
lucland <lucas.vsc@gmail.com>
2024-02-16 15:19:48 -0300

    Add CompanyProject model

---
+++ src/models/CompanyProject.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+class CompanyProject extends Model {
+    static init(sequelize) {
+      super.init({
+        company_id: {
+          type: DataTypes.STRING,
+          references: { model: 'companies', key: 'id' },
+        },
+        project_id: {
+          type: DataTypes.STRING,
+          references: { model: 'projects', key: 'id' },
+        },
+      }, {
+        sequelize,
+        modelName: 'CompanyProject',
+        tableName: 'company_projects',
+        timestamps: false,
+      });
+    }
+    static associate(models) {
+        this.belongsTo(models.Company, { foreignKey: 'company_id' });
+        this.belongsTo(models.Project, { foreignKey: 'project_id' });
+      }      
+  }
+
+module.exports = CompanyProject;
