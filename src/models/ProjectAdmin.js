709b3201368527cc2bc824e4e2d57da96800e7f9
lucland <lucas.vsc@gmail.com>
2024-02-16 15:20:13 -0300

    Add ProjectAdmin model

---
+++ src/models/ProjectAdmin.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+class ProjectAdmin extends Model {
+    static init(sequelize) {
+      super.init({
+        project_id: {
+          type: DataTypes.STRING,
+          references: { model: 'projects', key: 'id' },
+        },
+        user_id: {
+          type: DataTypes.STRING,
+          references: { model: 'users', key: 'id' },
+        },
+      }, {
+        sequelize,
+        modelName: 'ProjectAdmin',
+        tableName: 'project_admins',
+        timestamps: false,
+      });
+    }
+    static associate(models) {
+        this.belongsTo(models.Project, { foreignKey: 'project_id' });
+        this.belongsTo(models.User, { foreignKey: 'user_id' });
+      }
+      
+  }
+
+module.exports = ProjectAdmin;
