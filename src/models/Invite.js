5b3957fc20c6308bb1365ab2d8ee607bf934ead3
lucland <lucas.vsc@gmail.com>
2024-02-21 18:00:19 -0300

    Add Invite model class

---
+++ src/models/Invite.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Invite extends Model {
+    static init(sequelize) {
+      super.init({
+        id: {
+          type: DataTypes.STRING,
+          primaryKey: true,
+        },
+        email: DataTypes.STRING,
+        accepted: DataTypes.BOOLEAN,
+        sent: DataTypes.BOOLEAN,
+        thirdCompanyName: DataTypes.STRING,
+        dateSent: DataTypes.DATE,
+        viewed: DataTypes.BOOLEAN,
+      }, {
+        sequelize,
+        modelName: 'Invite',
+      });
+    }
+      
+}
+
+module.exports = Invite;
