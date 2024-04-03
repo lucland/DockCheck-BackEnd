ff561dd0bf27e43cb3605a560cbd63abf6736ba9
lucland <lucas.vsc@gmail.com>
2024-04-03 08:25:03 -0300

    Add employees_id field to Project model

--- src/models/Project.js
+++ src/models/Project.js
@@ -16,6 +16,7 @@ class Project extends Model {
       company_id: DataTypes.STRING,
       third_companies_id: DataTypes.ARRAY(DataTypes.STRING),
       admins_id: DataTypes.ARRAY(DataTypes.STRING),
+      employees_id: DataTypes.ARRAY(DataTypes.STRING),
       areas_id: DataTypes.ARRAY(DataTypes.STRING),
       is_docking: DataTypes.BOOLEAN,
       status: DataTypes.STRING,
