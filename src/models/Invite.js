fa4a4d99594d8fb95cfcd923ebe8b0ea4f75389b
lucland <lucas.vsc@gmail.com>
2024-04-02 21:05:15 -0300

    Update Invite model to include project_id field

--- src/models/Invite.js
+++ src/models/Invite.js
@@ -10,10 +10,10 @@ class Invite extends Model {
         email: DataTypes.STRING,
         accepted: DataTypes.BOOLEAN,
         sent: DataTypes.BOOLEAN,
-        project_id: DataTypes.STRING,
         thirdCompanyName: DataTypes.STRING,
         dateSent: DataTypes.DATE,
         viewed: DataTypes.BOOLEAN,
+        project_id: DataTypes.STRING,
       }, {
         sequelize,
         modelName: 'Invite',
